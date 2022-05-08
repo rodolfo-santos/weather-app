2a9779a6f7cf39238acff11098814a00d7daaeb9
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-05-08 16:08:32 -0300

    ♻ adding asyncData function with cookies

--- app.vue
+++ app.vue
@@ -1,37 +1,90 @@
 <script setup lang="ts">
-const search = ref("Toronto");
-const input = ref("");
+const cookie = useCookie("city");
+const config = useRuntimeConfig();
 
-const { data: city, error } = useFetch(
-  () =>
-    `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=abcf103caee413e6f9ee3bf766a5abd5`
+if (!cookie.value) cookie.value = "Toronto";
+
+const search = ref(cookie.value);
+const input = ref("");
+const background = ref("");
+
+const { data: city, error } = useAsyncData(
+  "city",
+  async () => {
+    let response;
+
+    try {
+      response = await $fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search.value}`,
+        {
+          params: {
+            units: "metric",
+            appid: config.WEATHER_APP_SECRET,
+          },
+        }
+      );
+
+      cookie.value = search.value;
+
+      const temp = response.main.temp;
+
+      if (temp <= -10) {
+        background.value =
+          "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > -10 && temp <= 0) {
+        background.value =
+          "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > 0 && temp <= 10) {
+        background.value =
+          "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+      } else {
+        background.value =
+          "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+      }
+    } catch (e) {}
+
+    return response;
+  },
+  {
+    watch: [search],
+  }
 );
 
-function handleClick(): void {
+const today = new Date().toLocaleDateString("en-US", {
+  weekday: "long",
+  year: "numeric",
+  mount: "long",
+  day: "numeric",
+});
+
+function handleClick() {
   const formatedSearch = input.value.trim().split(" ").join("+");
   search.value = formatedSearch;
   input.value = "";
 }
+
+function goBack() {
+  search.value = cookie.value;
+}
 </script>
 
 <template>
-  <div class="h-screen relative overflow-hidden">
-    <img src="" alt="" />
+  <div v-if="city" class="h-screen relative overflow-hidden">
+    <img :src="background" />
     <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
           <h1 class="text-7xl text-white">{{ city.name }}</h1>
-          <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
+          <p class="font-extralight text-2xl mt-2 text-white">{{ today }}</p>
           <img
-            :src="`https://openweathermap.org/img/w/${city.weather[0].icon}.png`"
-            alt=""
+            :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@4x.png`"
             class="w-56 icon"
           />
         </div>
         <div>
           <p class="text-9xl text-white font-extralight">
-            {{ city.main.temp }}º
+            {{ city.main.temp }}°
           </p>
         </div>
       </div>
@@ -48,6 +101,10 @@ function handleClick(): void {
       </div>
     </div>
   </div>
+  <div v-else class="p-1">
+    <h1 class="text-7xl">Opps, we cant1find that city</h1>
+    <button class="mt-5 bg-sky-400 px-10 w-50 text-white h-12">Go Back</button>
+  </div>
 </template>
 
 <style scoped>
@@ -57,6 +114,6 @@ function handleClick(): void {
 
 .icon {
   margin-left: -2.75rem;
-  margin-top: -2.175rem;
+  margin-top: -2.75rem;
 }
 </style>
