981078da8b56d86b025971cc5668827ad56890c1
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-05-08 15:08:56 -0300

    🚚 fetch openweather api

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+const search = ref("Toronto");
+const input = ref("");
+
+const { data: city, error } = useFetch(
+  () =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=abcf103caee413e6f9ee3bf766a5abd5`
+);
+
+function handleClick(): void {
+  const formatedSearch = input.value.trim().split(" ").join("+");
+  search.value = formatedSearch;
+  input.value = "";
+}
+</script>
+
 <template>
   <div class="h-screen relative overflow-hidden">
     <img src="" alt="" />
@@ -5,17 +21,30 @@
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
-          <h1 class="text-7xl text-white">Toronto</h1>
+          <h1 class="text-7xl text-white">{{ city.name }}</h1>
           <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
-          <img class="w-56 icon" src="" alt="" />
+          <img
+            :src="`https://openweathermap.org/img/w/${city.weather[0].icon}.png`"
+            alt=""
+            class="w-56 icon"
+          />
         </div>
         <div>
-          <p class="text-9xl text-white font-extralight">53ª</p>
+          <p class="text-9xl text-white font-extralight">
+            {{ city.main.temp }}º
+          </p>
         </div>
       </div>
       <div class="mt-20">
-        <input type="text" class="w-1/2 h-10" placeholder="Search a city..." />
-        <button class="bg-sky-400 w-20 text-white h-10">Search</button>
+        <input
+          type="text"
+          class="w-1/2 h-10"
+          placeholder="Search a city..."
+          v-model="input"
+        />
+        <button class="bg-sky-400 w-20 text-white h-10" @click="handleClick">
+          Search
+        </button>
       </div>
     </div>
   </div>
@@ -24,5 +53,10 @@
 <style scoped>
 .overlay {
   background-color: rgba(0, 0, 0, 0.5);
+}
+
+.icon {
+  margin-left: -2.75rem;
+  margin-top: -2.175rem;
 }
 </style>
