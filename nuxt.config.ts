24a65764226753195f048627825b97697f47ed61
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-05-08 14:13:08 -0300

    🎨 tailwind configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 import { defineNuxtConfig } from "nuxt";
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
